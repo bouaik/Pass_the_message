5def47528235ef0badb266a8ef62308da86b33f7
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-05 11:35:59 +0100

    show alert if user passing empty valmue

--- app.js
+++ app.js
@@ -5,17 +5,16 @@
         const message = document.getElementById('message');
         const value = message.value
 
-        document.querySelector('.messgae-content').textContent = value
-
-        message.value = ''
+        if (value === '') {
+            const feedback = document.querySelector('.feedback')
+            feedback.classList.add('show')
+            setTimeout( () => {
+                feedback.classList.remove('show')
+            }, 2000)
+        } else {
+            document.querySelector('.messgae-content').textContent = value
+            message.value = ''
+        }
     })
-
-
-
-
-
-
-
-
 
 })();
